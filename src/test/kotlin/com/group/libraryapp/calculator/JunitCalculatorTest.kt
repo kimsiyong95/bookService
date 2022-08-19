51845c6828dff25481b7b6b7b634ba0db27753d4
kimsiyong95 <kimsiyong95@gmail.com>
2022-08-19 15:43:21 +0900

    test : git cache rm and test code

--- src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -1,4 +1,40 @@
 package com.group.libraryapp.calculator
 
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class JunitCalculatorTest {
+
+    @Test
+    fun addTest(){
+        //given
+
+        val calulator = Calculator(5)
+
+        //when
+        calulator.add(3)
+        //then
+        assertThat(calulator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun divideExceptionTest(){
+        //given
+
+        val calculator = Calculator(5)
+
+
+        //when
+
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+
+
+
+        //then
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다.")
+    }
+
 }
